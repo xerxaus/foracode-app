d9a2fe41c7531a8f66fbe77e745936de6e30f7e8
xerxaus <kmrkzlcgl@gmail.com>
2026-05-07 12:50:18 +0300

    Update next.config.js
    vercel ayarı

--- next.config.js
+++ next.config.js
@@ -1,26 +1,12 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  productionBrowserSourceMaps: false,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
+    // Vercel'in ufak ESLint uyarılarında fişi çekmesini engeller
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.filename = 'static/chunks/[name]-[contenthash:8].js';
-      config.output.chunkFilename = 'static/chunks/[contenthash:16].js';
-    }
-    return config;
+    // Vercel'in TypeScript hatalarını görmezden gelmesini sağlar
+    ignoreBuildErrors: true,
   },
 };
 
