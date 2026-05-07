b599d2048fa96a351021db9a2071e25ab0c7679e
xerxaus <kmrkzlcgl@gmail.com>
2026-05-07 16:11:24 +0300

    Update next.config.js
    50mb

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Vercel'in ufak ESLint uyarılarında fişi çekmesini engeller
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Vercel'in TypeScript hatalarını görmezden gelmesini sağlar
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
   },
 };
 
