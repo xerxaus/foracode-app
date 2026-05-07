03b16d2adf350fe596bc54576d9c2ca847b14be8
xerxaus <kmrkzlcgl@gmail.com>
2026-05-07 17:00:59 +0300

    Update aws-config.ts
    s3 baglanti kodlari eklendi

--- lib/aws-config.ts
+++ lib/aws-config.ts
@@ -8,5 +8,13 @@ export function getBucketConfig() {
 }
 
 export function createS3Client() {
-  return new S3Client({});
+  return new S3Client({
+    region: process.env.AWS_REGION,
+    endpoint: process.env.AWS_ENDPOINT,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    },
+    forcePathStyle: true,
+  });
 }
